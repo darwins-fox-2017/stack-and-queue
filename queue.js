81f4051e8a71842b9e3c492f1aee2a7ef6d89bd7
isumizumi <isumi.karina72@gmail.com>
2017-02-01 14:18:45 +0700

    Finish

--- queue.js
+++ queue.js
@@ -2,6 +2,54 @@
 
 class Queue {
   constructor () {
+    this.data = []
+    this.maxSize = 5
+  }
 
+  queuePush (x) {
+    if (this.isEmpty() === "Yes") {
+      return `${this.data.push(x)} record added.\n`
+    }
+    else if (this.data.length > this.maxSize) {
+      return "Queue is Full!\n"
+    }
+  }
+
+  queueShift () {
+    if (this.data.length === 0) {
+      return "Data is empty!\n"
+    } else {
+      return `${this.data.shift()} deleted.\n`
+    }
+  }
+
+  queuePeek () {
+    return this.data[this.data.length-1]
+  }
+
+  isEmpty () {
+    if (this.data.length < this.maxSize) {
+      return "Yes"
+    } else {
+      return "Queue is Full!\n"
+    }
   }
 }
+
+let myQueue = new Queue()
+
+console.log(`The queue is empty? ${myQueue.isEmpty()}\n`)
+myQueue.queuePush("Javascript")
+myQueue.queuePush("is just so")
+myQueue.queuePush("cool")
+
+myQueue.queuePush(15)
+myQueue.queuePeek()
+
+console.log(myQueue.queueShift())
+console.log(myQueue.queueShift())
+console.log(myQueue.queueShift())
+console.log(myQueue.queueShift())
+
+console.log(`The queue is empty? ${myQueue.isEmpty()}\n`)
+console.log(myQueue.queueShift())
--- stack.js
+++ stack.js
@@ -2,18 +2,54 @@
 
 class Stack {
   constructor () {
+    this.data = []
+    this.maxSize = 5
   }
 
   stackPush (x) {
+    if (this.isEmpty() === "Yes") {
+      return `${this.data.push(x)} record added.\n`
+    }
+    else if (this.data.length > this.maxSize) {
+      return "Stack is Full!\n"
+    }
   }
 
   stackPop () {
+    if (this.data.length === 0) {
+      return "Data is empty!\n"
+    } else {
+      return `${this.data.pop()} deleted.\n`
+    }
   }
 
   stackPeek () {
-
+    return this.data[this.data.length-1]
   }
 
   isEmpty () {
+    if (this.data.length < this.maxSize) {
+      return "Yes"
+    } else {
+      return "Stack is Full!\n"
+    }
   }
 }
+
+let myStack = new Stack()
+
+console.log(`The stack is empty? ${myStack.isEmpty()}\n`)
+myStack.stackPush("Javascript")
+myStack.stackPush("is just so")
+myStack.stackPush("cool")
+
+myStack.stackPush(15)
+myStack.stackPeek()
+
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+
+console.log(`The stack is empty? ${myStack.isEmpty()}\n`)
+console.log(myStack.stackPop())
