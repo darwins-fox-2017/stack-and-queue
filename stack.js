de33332600972f22e90bb2d68acd0885806351a9
Diky Arga <dikyarga.id@gmail.com>
2017-02-01 11:18:09 +0700

    move to another computer

--- stack.js
+++ stack.js
@@ -1,19 +1,28 @@
 'use strict'
 
 class Stack {
-  constructor () {
+  constructor (value) {
+    this.stack = value
   }
 
   stackPush (x) {
+    this.stack.push(x)
   }
 
   stackPop () {
+    this.stack.pop()
   }
 
   stackPeek () {
-
+    return this.stack.shift()
   }
 
   isEmpty () {
+    return this.stack.length == 0 ? true : false
   }
 }
+
+var tumpukanPiring = new Stack('Piring Kecil')
+console.log(tumpukanPiring);
+tumpukanPiring.stackPush('Piring Terbang')
+console.log(tumpukanPiring);
