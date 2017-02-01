658961e3629acc8e98344aad537cb1aa3e55e027
Samuel PH Sinaga <samuelphsinaga@gmail.com>
2017-02-01 14:10:38 +0700

    release 2 - queue in array

--- queue.js
+++ queue.js
@@ -4,5 +4,24 @@ class Queue {
   constructor (value) {
     this.queue = [value]
   }
-  
+  queuePush(x){
+    this.queue.push(x)
+  }
+
+  queuePop(){
+    this.queue.shift()
+  }
+
+
 }
+
+var antrianBus = new Queue('bus 1')
+
+console.log(antrianBus);
+
+antrianBus.queuePush('bus 2')
+console.log('setelah ditambah satu bus : ', antrianBus);
+antrianBus.queuePush('bus 3')
+console.log('setelah ditambah satu bus : ', antrianBus);
+antrianBus.queuePop()
+console.log('Setelah di kurang satu bus : ', antrianBus);
