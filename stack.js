a624f2aa045aa17a8b39b7147698c01bcc7f2448
Samuel PH Sinaga <samuelphsinaga@gmail.com>
2017-02-01 12:00:00 +0700

    release 1 - add MAX_SIZE (Setter and Getter)

--- stack.js
+++ stack.js
@@ -2,11 +2,24 @@
 
 class Stack {
   constructor (value) {
-    this.stack = value
+    this.stack = [value]
+    this.MAX_SIZE = 3
   }
 
   stackPush (x) {
-    this.stack.push(x)
+    if (!this.isFull) {
+      console.log(`Sorry, you can't add more data to the stack, coz it already full!`);
+    } else {
+      this.stack.push(x)
+    }
+  }
+
+  setMaxSize (x) {
+    this.MAX_SIZE = x
+  }
+
+  getMaxSize (){
+    return this.MAX_SIZE
   }
 
   stackPop () {
@@ -14,15 +27,34 @@ class Stack {
   }
 
   stackPeek () {
-    return this.stack.shift()
+    return this.stack[this.stack.length -1]
   }
 
   isEmpty () {
     return this.stack.length == 0 ? true : false
+  }
+
+  isFull () {
+    console.log('isinya berapa ? ' + this.stack.length);
+    return this.stack.length >= this.MAX_SIZE ? true : false
+  }
+  print() {
+
   }
 }
 
 var tumpukanPiring = new Stack('Piring Kecil')
 console.log(tumpukanPiring);
 tumpukanPiring.stackPush('Piring Terbang')
-console.log(tumpukanPiring);
+console.log('Apakah udah penuh ? ' + tumpukanPiring.isFull());
+console.log('Sesudah tambah data : ', tumpukanPiring);
+tumpukanPiring.stackPush('Piringan Kaset')
+console.log('Sesudah tambah data lagi : ', tumpukanPiring);
+console.log('stack peek : ' + tumpukanPiring.stackPeek());
+
+console.log('Apakah udah penuh ? ' + tumpukanPiring.isFull());
+console.log('Sedudah di pop satu : ', tumpukanPiring);
+tumpukanPiring.stackPop()
+tumpukanPiring.stackPop()
+console.log('Sedudah di pop lagi : ', tumpukanPiring);
+console.log(`The stack is empty ? ${tumpukanPiring.isEmpty()}`);
