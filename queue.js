925fe678e99a2159f46a54a876a3110212c1b333
Eri Irawan <eryirawan91@gmail.com>
2017-02-01 14:48:05 +0700

    stack & queue done

--- queue.js
+++ queue.js
@@ -2,6 +2,35 @@
 
 class Queue {
   constructor () {
+    this.arr = []
+  }
 
+  queuePush(val){
+    return this.arr.push(val)
+  }
+
+  queueShift(){
+    return this.arr.shift()
+  }
+
+  queuePeek(){
+    return this.arr.shift()
+  }
+
+  isEmpty(){
+    if(this.arr.length === 0){
+      return 'is Empty'
+    }else {
+      return this.queuePeek()
+    }
   }
 }
+
+var resultQueue = new Queue();
+    resultQueue.queuePush('queue one')
+    resultQueue.queuePush('queue two')
+    resultQueue.queuePush('queue three')
+
+console.log(resultQueue.queueShift());
+console.log(resultQueue.queuePeek());
+console.log(resultQueue.isEmpty());
--- stack.js
+++ stack.js
@@ -2,18 +2,35 @@
 
 class Stack {
   constructor () {
+    this. arr = []
   }
 
-  stackPush (x) {
+  stackPush (val) {
+    return this.arr.push(val)
   }
 
   stackPop () {
+    return this.arr.pop()
   }
 
   stackPeek () {
-
+    return this.arr.pop()
   }
 
   isEmpty () {
+    if(this.arr.length === 0){
+      return 'is Empty'
+    }else {
+      return this.stackPeek()
+    }
   }
 }
+
+var resultStack = new Stack()
+    resultStack.stackPush("stack one")
+    resultStack.stackPush("stack two")
+    resultStack.stackPush("stack three")
+
+console.log(resultStack.stackPop());
+console.log(resultStack.stackPeek());
+console.log(resultStack.isEmpty());
