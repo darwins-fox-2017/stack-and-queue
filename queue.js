45354aa7b80fd52d43ab188760a2e4f2fb760822
hepiska <ego1403@gmail.com>
2017-02-01 15:56:41 +0700

    stack and queue ego

--- queue.js
+++ queue.js
@@ -1,7 +1,57 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor (data=[]) {
+    this.data=data;
+    //this.maxZise=3;
+  }
+  stackInsert (x) {
+    if (this.data.length<this.maxZise) {
+      this.data.push(x);
+    }else {
+      console.log('stack full!');
+    }
+    return this
+  }
+  stackSetMax(x){
+    this.maxZise=x;
+    return this;
+   }
+ }
+ stackRemove () {
+   this.data.shift();
+   return this;
+ }
 
+stackPeek () {
+  let firstData=0;
+  return this.data[firstData];
+}
+
+isEmpty () {
+  if (this.data.length==0) {
+    return true;
+  }else{
+    return false;
   }
 }
+
+isFull(){
+  if (this.data.length==this.maxZise) {
+    return true;
+  }else{
+    return false;
+  }
+}
+
+}
+
+let mydata= new Queue();
+
+console.log(`the stack is empety ? ${mydata.isEmpty()}`);
+mydata.stackInsert(3).stackInsert('hai').stackInsert(['apa','kabar']);
+console.log(`the stack is full ? ${mydata.isFull()}`);
+console.log('data pertama : ' +mydata.stackPeek());
+mydata.stackInsert('lima');
+mydata.stackRemove().stackRemove().stackRemove(); 
+console.log(`the stack is empety ? ${mydata.isEmpty()}`);
