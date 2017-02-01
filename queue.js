d7c7a5e109d90ac52005c22963ffaef500ab13c8
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-01 11:43:36 +0700

    Done all release

--- queue.js
+++ queue.js
@@ -2,6 +2,68 @@
 
 class Queue {
   constructor () {
-
+    this.stack = []
+    this.max = 5
   }
+
+  queueInsert (x) {
+    if(!this.isFull()){
+      this.stack.push(x)
+      console.log(`${x} has been added`)
+    }else {
+      console.log(`stack is full `)
+    }
+  }
+
+  queueRemove () {
+    let word = this.stack[0]
+    this.stack.shift()
+    return `${word} has been removed`
+  }
+
+  queuePeek () {
+    console.log(this.stack[this.stack.length-1])
+  }
+
+  isEmpty () {
+    if(!this.stack.length){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  set(x){
+    this.max = x
+  }
+
+  getMaxSize(){
+    console.log(this.max)
+  }
+
+  isFull(){
+    if(this.stack.length == this.max){
+      return true
+    }
+    return false
+  }
+
 }
+
+
+var myQueue = new Queue()
+
+console.log(`The stack is empty? ${myQueue.isEmpty()}`);
+
+myQueue.queueInsert("Javascript")
+myQueue.queueInsert("is just so")
+myQueue.queueInsert("cool")
+myQueue.queueInsert(15)
+myQueue.queueInsert(9)
+myQueue.queueInsert(5)
+
+myQueue.queueRemove() // remove element pertama pada array
+myQueue.queuePeek()
+
+console.log(myQueue.isFull())
+console.log(`The stack is empty? ${myQueue.isEmpty()}`)
--- stack.js
+++ stack.js
@@ -2,18 +2,67 @@
 
 class Stack {
   constructor () {
+    this.stack = []
+    this.max = 5
   }
 
   stackPush (x) {
+    if(!this.isFull()){
+      this.stack.push(x)
+      console.log(`${x} has been added`)
+    }else {
+      console.log(`stack is full `)
+    }
   }
 
   stackPop () {
+    let word = this.stack[this.stack.length - 1]
+    this.stack.pop()
+    return `${word} has been removed`
   }
 
   stackPeek () {
-
+    console.log(this.stack[this.stack.length-1])
   }
 
   isEmpty () {
+    if(!this.stack.length){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  set(x){
+    this.max = x
+  }
+
+  getMaxSize(){
+    console.log(this.max)
+  }
+
+  isFull(){
+    if(this.stack.length == this.max){
+      return true
+    }
+    return false
   }
 }
+
+var myStack = new Stack()
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`)
+
+myStack.stackPush("Javascript")
+myStack.stackPush("is just so")
+myStack.stackPush("cool")
+myStack.stackPush(15)
+myStack.stackPush(9)
+myStack.stackPush(5)
+
+myStack.stackPeek()
+
+myStack.stackPop()
+
+console.log(myStack.isFull())
+console.log(`The stack is empty? ${myStack.isEmpty()}`)
