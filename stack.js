d81a8099f2a5cf0ca61aa1760fff6d317d812d05
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-02-02 10:19:50 +0700

    Danang stack and queue

--- stack.js
+++ stack.js
@@ -2,18 +2,49 @@
 
 class Stack {
   constructor () {
+    this.stack = []
+    this._MAX_SIZE = 5
   }
 
   stackPush (x) {
+    if (this.isFull()) console.log('Array is full')
+    else this.stack[this.stack.length] = x
   }
 
   stackPop () {
+    this.stack.splice(this.stack.length-1, 1)
   }
 
   stackPeek () {
-
+    return this.stack[this.stack.length-1]
   }
 
   isEmpty () {
+    return this.stack.length === 0
+  }
+
+  setMax (maxValue) {
+    this._MAX_SIZE = maxValue
+  }
+
+  getMax () {
+    return this._MAX_SIZE
+  }
+
+  isFull () {
+    return this.stack.length === this._MAX_SIZE
   }
 }
+
+const stack = new Stack()
+console.log(stack.isEmpty())
+stack.stackPush('Stack 1')
+stack.stackPush('Stack 2')
+stack.stackPush('Stack 3')
+stack.stackPush('Stack 4')
+stack.stackPush('Stack 5')
+stack.stackPush('Stack 6')
+console.log(stack.stackPeek())
+stack.stackPop()
+console.log(stack.isEmpty())
+console.log(stack.stack);
